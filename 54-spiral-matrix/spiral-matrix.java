5d641c22fb9f463469cc0c42d55f71fcdd2ecab6
Harsh kumar <harshkumar9720218944@gmail.com>
2026-07-30 23:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (43.42%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,37 +1,44 @@
+
+
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return result;
+
         int top = 0;
         int bottom = matrix.length - 1;
         int left = 0;
         int right = matrix[0].length - 1;
 
-        while(top <= bottom && left <= right){
-            for(int j = left; j <= right; j++){
-                result.add(matrix[top][j]);
+        while (top <= bottom && left <= right) {
+            
+            for (int col = left; col <= right; col++) {
+                result.add(matrix[top][col]);
             }
-            top++;
+            top++; 
 
-            for(int i = top; i <= bottom; i++){
-                result.add(matrix[i][right]);
+            
+            for (int row = top; row <= bottom; row++) {
+                result.add(matrix[row][right]);
             }
-            right--;
+            right--; 
 
-            if(top <= bottom){
-                for(int j = right; j >= left; j--){
-                    result.add(matrix[bottom][j]);
+            if (top <= bottom) {
+                for (int col = right; col >= left; col--) {
+                    result.add(matrix[bottom][col]);
                 }
-                bottom--;
+                bottom--; 
             }
 
-            if(left <= right){
-                for(int i = bottom; i >= top; i--){
-                    result.add(matrix[i][left]);
+            
+            if (left <= right) {
+                for (int row = bottom; row >= top; row--) {
+                    result.add(matrix[row][left]);
                 }
-                left++;
+                left++; 
             }
         }
+
         return result;
-        
     }
 }
